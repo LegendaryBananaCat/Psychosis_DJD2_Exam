0b2d94817bcd746f2f4d882a9c183b34878daf9b
Joana Silva <joana.f.silva.99@gmail.com>
2020-02-09 20:42:35 +0000

    Added Padlock

---
+++ Assets/Scripts/Interactions/CodeLock.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeLock : MonoBehaviour
+{
+    int codeLength;
+    int placeInCode;
+
+    public string code = "";
+    public string attemptedCode;
+
+    public Transform toOpen;
+
+    private Animator _animator;
+
+    public GameObject WrongPanel;
+
+    private void Start()
+    {
+        _animator = GetComponentInParent<Animator>();
+        codeLength = code.Length;
+    }
+
+    void CheckCode()
+    {
+        if(attemptedCode == code)
+        {
+            _animator.SetBool("Open", true);
+        }
+        else
+        {
+            StartCoroutine(ShowWrongInputMessage());
+           
+        }
+    }
+
+    IEnumerator ShowWrongInputMessage()
+    {
+        WrongPanel.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+
+        WrongPanel.SetActive(false); ;
+    }
+
+    public void SetValue(string value)
+    {
+        placeInCode++;
+
+        if(placeInCode <= codeLength)
+        {
+            attemptedCode += value;
+        }
+
+        if(placeInCode == codeLength)
+        {
+            CheckCode();
+
+            attemptedCode = "";
+            placeInCode = 0;
+        }
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -27,8 +27,7 @@ public class MainMenu : MonoBehaviour
         mainMenu.SetActive(false);
         camAnim.SetBool("Change", true);
         camAnim.SetBool("Controls", true);
-
-        controlMenu.SetActive(true);
+        StartCoroutine(ControlsMenu());
     }
 
     public void Pages()
@@ -37,7 +36,7 @@ public class MainMenu : MonoBehaviour
         camAnim.SetBool("Change", true);
         camAnim.SetBool("Pages", true);
 
-        pagesMenu.SetActive(true);
+        StartCoroutine(PagesMenu());
     }
 
     public void Exit()
@@ -46,8 +45,7 @@ public class MainMenu : MonoBehaviour
         camAnim.SetBool("Change", true);
         camAnim.SetBool("Exit", true);
 
-        Debug.Log("Exit");
-        Application.Quit();
+        StartCoroutine(ExitTime());
     } 
 
     public void Menu()
@@ -58,6 +56,31 @@ public class MainMenu : MonoBehaviour
         controlMenu.SetActive(false);
         pagesMenu.SetActive(false);
 
+        StartCoroutine(MMMenu());
+    }
+
+    IEnumerator MMMenu()
+    {
+        yield return new WaitForSeconds(2);
         mainMenu.SetActive(true);
+    }
+
+    IEnumerator ControlsMenu()
+    {
+        yield return new WaitForSeconds(2);
+        controlMenu.SetActive(true);
+    }
+
+    IEnumerator PagesMenu()
+    {
+        yield return new WaitForSeconds(2);
+        pagesMenu.SetActive(true);
+    }
+
+    IEnumerator ExitTime()
+    {
+        yield return new WaitForSeconds(2);
+        Debug.Log("Exit");
+        Application.Quit();
     }
 }

426e66fcf644bb43a10e388c91edb401d9b08eb0
Joana Silva <joana.f.silva.99@gmail.com>
2020-02-11 07:59:44 +0000

    Fixed Obj PickUp

---
+++ Assets/Scripts/Interactions/ObjPickUp.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class ObjPickUp : MonoBehaviour
+{
+    public float objDistance;
+    public GameObject actionDisplay;
+    public GameObject actionText;
+    public GameObject indicCross;
+    public TMP_Text newText;
+    //public AudioSource doorSound;
+
+    private bool pickedUp = false;
+    public bool BackDKey;
+    public bool FrontDKey;
+    public bool Pliers;
+
+    public GameObject pickedObjectGroup;
+    public GameObject pickedObject;
+    public GameObject objPlayer;
+
+    void Update()
+    {
+        objDistance = PlayerInteract.TargetDistance;
+
+        if (Input.GetButtonDown("Drop"))
+        {
+            DropObj();
+            pickedUp = false;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (objDistance <= 2)
+        {
+            
+            indicCross.SetActive(true);
+            actionDisplay.SetActive(true);
+            actionText.SetActive(true);
+
+            if(FrontDKey == true)
+            {
+                newText.SetText("Pick Up Front Door Key");
+                pickedUp = true;
+            }
+
+            if (BackDKey == true)
+            {
+                newText.SetText("Pick Up Back Door Key");
+            }
+
+            if (Pliers == true)
+            {
+                newText.SetText("Pick Up Pliers");
+            }
+        }
+
+        else
+        {
+            newText.SetText(" ");
+            actionDisplay.SetActive(false);
+            actionText.SetActive(false);
+        }
+
+        if (Input.GetButtonDown("Action"))
+        {
+            if (objDistance <= 2)
+            {
+                if (pickedUp == false)
+                {
+                    PickUpObj();
+                }
+
+                if (pickedUp == true)
+                {
+                    Debug.Log("Yes");
+                    DropObj();
+                    PickUpObj();
+                }
+            }  
+        }
+    }
+
+    void OnMouseExit()
+    {
+        indicCross.SetActive(false);
+        actionDisplay.SetActive(false);
+        actionText.SetActive(false);
+    }
+
+     void PickUpObj()
+     {
+        pickedUp = true;
+        newText.SetText(" ");
+        this.GetComponent<BoxCollider>().enabled = false;
+        actionDisplay.SetActive(false);
+        actionText.SetActive(false);
+        pickedObject.SetActive(false);
+        objPlayer.SetActive(true);
+     }
+
+    void DropObj()
+    {
+        if (pickedUp == false)
+        {
+            return;
+        }
+
+        pickedUp = false;
+        pickedObject.SetActive(true);
+        objPlayer.SetActive(false);
+        pickedObjectGroup.transform.position = new Vector3(objPlayer.transform.position.x, -0.0147f, objPlayer.transform.position.z);
+        this.GetComponent<BoxCollider>().enabled = true;
+        StartCoroutine(OriginPos());
+    }
+
+    IEnumerator OriginPos()
+    {
+        if (FrontDKey == true)
+        {
+            yield return new WaitForSeconds(15);
+
+            pickedObjectGroup.transform.position = new Vector3(-7.045f, 0.681f, -22.69f);
+        }
+
+        if (BackDKey == true)
+        {
+            yield return new WaitForSeconds(15);
+
+            pickedObjectGroup.transform.position = new Vector3(25.543f, -0.0147f, -7.654f);
+        }
+
+        if (Pliers == true)
+        {
+            yield return new WaitForSeconds(15);
+
+            pickedObjectGroup.transform.position = new Vector3(-4.8195f, 0.1357f, -14.6125f);
+        }
+    }
+}
--- Assets/Scripts/Interactions/PickupObjs.cs
+++ Assets/Scripts/Interactions/PickupObjs.cs
@@ -7,98 +7,98 @@ using TMPro;
 public class PickupObjs : MonoBehaviour
 {
 
-    public float objDistance;
-    public GameObject actionDisplay;
-    public GameObject actionText;
-    public GameObject indicCross;
-    public TMP_Text newText;
-    //public AudioSource doorSound;
+        //public float objDistance;
+        //public GameObject actionDisplay;
+        //public GameObject actionText;
+        //public GameObject indicCross;
+        //public TMP_Text newText;
+        ////public AudioSource doorSound;
 
-    public GameObject pickedObj;
-    public GameObject newObj;
-    public GameObject fakeObj;
-    private bool pickedUp;
+        //public GameObject pickedObj;
+        //public GameObject newObj;
+        //public GameObject fakeObj;
+        //private bool pickedUp;
 
-    private void Start()
-    {
-        pickedUp = false;
-    }
-    void Update()
-    {
-        objDistance = PlayerInteract.TargetDistance;
+        //private void Start()
+        //{
+        //    pickedUp = false;
+        //}
+        //void Update()
+        //{
+        //    objDistance = PlayerInteract.TargetDistance;
 
-        ObjsDistance();
-    }
+        //    ObjsDistance();
+        //}
 
-    void OnMouseExit()
-    {
-        indicCross.SetActive(false);
-        actionDisplay.SetActive(false);
-        actionText.SetActive(false);
-    }
-    
-    void ObjsDistance()
-    {
-        if (Input.GetButtonDown("Drop"))
-        {
-            DropObj();
-        }
+        //void OnMouseExit()
+        //{
+        //    indicCross.SetActive(false);
+        //    actionDisplay.SetActive(false);
+        //    actionText.SetActive(false);
+        //}
 
-        if (objDistance <= 2 && pickedUp == false)
-        {
-            indicCross.SetActive(true);
-            newText.SetText("Pick Up");
-            actionDisplay.SetActive(true);
-            actionText.SetActive(true);
-        }
-        else
-        {
-            newText.SetText(" ");
-            actionDisplay.SetActive(false);
-            actionText.SetActive(false);
-        }
+        //void ObjsDistance()
+        //{
+        //    if (Input.GetButtonDown("Drop"))
+        //    {
+        //        DropObj();
+        //    }
 
-        if (Input.GetButtonDown("Action"))
-        {
-            if (objDistance <= 2)
-            {
-                if (pickedUp == false)
-                {
-                    PickUp();
-                }
+        //    if (objDistance <= 2 && pickedUp == false)
+        //    {
+        //        indicCross.SetActive(true);
+        //        newText.SetText("Pick Up");
+        //        actionDisplay.SetActive(true);
+        //        actionText.SetActive(true);
+        //    }
+        //    else
+        //    {
+        //        newText.SetText(" ");
+        //        actionDisplay.SetActive(false);
+        //        actionText.SetActive(false);
+        //    }
 
-                if (pickedUp == true)
-                {
-                    DropObj();
-                    PickUp();
-                }
-            }
-        }
+        //    if (Input.GetButtonDown("Action"))
+        //    {
+        //        if (objDistance <= 2)
+        //        {
+        //            if (pickedUp == false)
+        //            {
+        //                PickUp();
+        //            }
 
-    }
+        //            if (pickedUp == true)
+        //            {
+        //                DropObj();
+        //                PickUp();
+        //            }
+        //        }
+        //    }
 
-    void PickUp()
-    {
-        newText.SetText(" ");
-        pickedUp = true;
-        this.GetComponent<BoxCollider>().enabled = false;
-        actionDisplay.SetActive(false);
-        actionText.SetActive(false);
-        fakeObj.SetActive(false);
-        newObj.SetActive(true);
-    }
+        //}
 
-    void DropObj()
-    {
-        if (pickedUp == false)
-        {
-            return;
-        }
+        //void PickUp()
+        //{
+        //    newText.SetText(" ");
+        //    pickedUp = true;
+        //    this.GetComponent<BoxCollider>().enabled = false;
+        //    actionDisplay.SetActive(false);
+        //    actionText.SetActive(false);
+        //    fakeObj.SetActive(false);
+        //    newObj.SetActive(true);
+        //}
 
-        pickedObj.transform.position = new Vector3(newObj.transform.position.x, newObj.transform.position.y + 1, newObj.transform.position.z);
-        pickedUp = false;
-        fakeObj.SetActive(true);
-        newObj.SetActive(false);
-        this.GetComponent<BoxCollider>().enabled = true;
-    }
+        //void DropObj()
+        //{
+        //    if (pickedUp == false)
+        //    {
+        //        return;
+        //    }
+
+        //    pickedObj.transform.position = new Vector3(newObj.transform.position.x, newObj.transform.position.y + 1, newObj.transform.position.z);
+        //    pickedUp = false;
+        //    fakeObj.SetActive(true);
+        //    newObj.SetActive(false);
+        //    this.GetComponent<BoxCollider>().enabled = true;
+        //}
 }
